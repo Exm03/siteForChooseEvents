a1a471057a3c199c00fa72679a760c6ebc4af519
Yan <Exm230600@gmail.com>
2023-06-13 00:16:55 +0200

    some anims

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
 let startBtn = document.querySelector('.start-batn');
 let section = document.querySelector('.section');
 
-startBtn.addEventListener('click', function () {
+function innerBox() {
+
     section.innerHTML = `<h2>Выбор активности. <br>Можно выбрать только что-то одно</br></h2>
     <article class="article">
         <button class='glowing-btn glowing-batn' id= 'картинг'><span class='glowing-txt glowing-text' id= картинг>Двух уро<span class='faulty-letter' id= картинг>вневы</span>й карт<span class='faulty-letter' id= картинг>инг</span></button>
@@ -10,7 +11,16 @@ startBtn.addEventListener('click', function () {
         <button class='glowing-btn glowing-batn' id= 'Wake board'><span class='glowing-txt glowing-text'id= 'Wake board'>wak<span class='faulty-letter'id= 'Wake board'>e bo</span>ard</button>
         <button class='glowing-btn glowing-batn' id= 'Мини-гольф'><span class='glowing-txt glowing-text' id= 'Мини-гольф'>игра<span class='faulty-letter'id= 'Мини-гольф'> в ми</span>ни-го<span class='faulty-letter'id= 'Мини-гольф'>льф</span></button>
     </article>`
+    section.classList.remove('slide-out-top')
+    section.classList.add('slide-in-bottom')
     checkEvents()
+}
+
+startBtn.addEventListener('click', function () {
+    section.classList.add('slide-out-top')
+
+    setTimeout(innerBox, 600)
+    
 })
 
 let newBats = document.querySelectorAll('.glowing-batn');
@@ -22,21 +32,33 @@ let bats = document.querySelectorAll('.bat');
 let placeChange = ''
 
 function checkPlace(eventChange, eatsChange) {
+    section.classList.remove('slide-in-bottom', 'slide-in-br', 'slide-in-left')
     bats = document.querySelectorAll('.bat');
     bats.forEach(btn => {
         btn.addEventListener('click', function (event) {
+            section.classList.add('slide-out-bck-cente')
             placeChange = event.target.id
-            section.innerHTML = ` <h2>На этом все <br>Вот что ты выбрала</h2>
-            <article class= "article">
-            <button class='glowing-btn glowing-batn first'><span class='glowing-txt glowing-text'>${eventChange}</button>
-                <button class='glowing-btn glowing-batton second'><span class='glowing-txt glowing-texts'>${eatsChange}</button>
-                <button class='glowing-btn bat third'><span class='glowing-txt'>${placeChange}</button>
-                </article>
-                <h2>Сдейлай скриншот или фото и пришли мне</h2>
-            `
+            
+            setTimeout(animForSect, 600, eventChange, eatsChange, placeChange)
         })
     })
+
+ }
+
+function animForSect (eventChange, eatsChange, placeChange) {
+    section.classList.remove('slide-out-bck-cente')
+    
+    section.classList.add('scale-in-center')
+    section.innerHTML = ` <h2>На этом все <br>Вот что ты выбрала</h2>
+    <article class= "article">
+    <button class='glowing-btn glowing-batn first'><span class='glowing-txt glowing-text'>${eventChange}</button>
+        <button class='glowing-btn glowing-batton second'><span class='glowing-txt glowing-texts'>${eatsChange}</button>
+        <button class='glowing-btn bat third'><span class='glowing-txt'>${placeChange}</button>
+        </article>
+        <h2>Сдейлай скриншот или фото и пришли мне</h2>
+    `
 }
+
 
 let eatsChange = '';
 
@@ -45,7 +67,17 @@ function checkEats(eventChange) {
     btns.forEach(btn => {
         btn.addEventListener('click', function (event) {
             eatsChange = event.target.id
-            section.innerHTML = ` <h2 >Выбор места для поездки за город. <br>Можно выбрать только что-то одно</br></h2>
+            
+            section.classList.add('slide-out-tl')
+            setTimeout(innerBoxEventsChek, 600, eventChange, eatsChange)
+        })
+    })
+}
+
+function innerBoxEventsChek (eventChange, eatsChange) {
+    section.classList.remove('slide-out-tl')
+    section.classList.add('slide-in-br')
+    section.innerHTML = ` <h2 >Выбор места для поездки за город. <br>Можно выбрать только что-то одно</br></h2>
             <article class="article palce-art">
                 <button class='glowing-btn bat' id= 'Озеро Свитязь'><span class='glowing-txt'id= 'Озеро Свитязь'>Озе<span class='faulty-letter'id= 'Озеро Свитязь'>ро св</span>итязь</button>
                 <button class='glowing-btn bat' id= 'Браславские озера'><span class='glowing-txt'id= 'Браславские озера'>Брас<span class='faulty-letter' id= 'Браславские озера'>лавск</span>ие оз<span class='faulty-letter'id= 'Браславские озера'>ера</span></button>
@@ -56,8 +88,6 @@ function checkEats(eventChange) {
             </article>`
             console.log(eventChange)
     checkPlace(eventChange, eatsChange)
-        })
-    })
 }
 
 let eventChange = ''
@@ -66,18 +96,25 @@ function checkEvents() {
     newBats.forEach(btn => {
         btn.addEventListener('click', function (event) {
             eventChange = event.target.id
-            section.innerHTML = `<h2>Выбор заведения для посидеть и покушать. <br>Можно выбрать только что-то одно</br></h2>
-        
-            <article class="article">
-                <button class='glowing-btn glowing-batton' id= 'Кальянная'><span class='glowing-txt glowing-texts'id= 'Кальянная'>Каль<span class='faulty-letter'id= 'Кальянная'>янн</span>ая</button>
-                <button class='glowing-btn glowing-batton' id= 'Бар'><span class='glowing-txt glowing-texts' id= 'Бар'>Б<span class='faulty-letter' id= 'Бар'>ар</span></button>
-                <button class='glowing-btn glowing-batton' id= 'Ресторан'><span class='glowing-txt glowing-texts' id= 'Ресторан'>Рес<span class='faulty-letter' id= 'Ресторан'>тор</span>ан</button>
-                <button class='glowing-btn glowing-batton' id= 'Кофейня'><span class='glowing-txt glowing-texts' id= 'Кофейня'>Коф<span class='faulty-letter' id= 'Кофейня'>ей</span>ня</button>
-                <button class='glowing-btn glowing-batton' id= 'Кафе с завтраками'><span class='glowing-txt glowing-texts'id= 'Кафе с завтраками'>кафе<span class='faulty-letter'id= 'Кафе с завтраками'> с завт</span>рака<span class='faulty-letter'id= 'Кафе с завтраками'>ми</span></button>
-            </article> 
-    `
-    checkEats(eventChange)
+            section.classList.add('slide-out-right')
+            setTimeout (innerBoxEvents, 600, eventChange)
+    
         })
     }) 
 }
 
+function innerBoxEvents(eventChange) {
+    section.classList.remove('slide-out-right')
+    section.classList.add('slide-in-left')
+    section.innerHTML = `<h2>Выбор заведения для посидеть и покушать. <br>Можно выбрать только что-то одно</br></h2>
+        
+    <article class="article">
+        <button class='glowing-btn glowing-batton' id= 'Кальянная'><span class='glowing-txt glowing-texts'id= 'Кальянная'>Каль<span class='faulty-letter'id= 'Кальянная'>янн</span>ая</button>
+        <button class='glowing-btn glowing-batton' id= 'Бар'><span class='glowing-txt glowing-texts' id= 'Бар'>Б<span class='faulty-letter' id= 'Бар'>ар</span></button>
+        <button class='glowing-btn glowing-batton' id= 'Ресторан'><span class='glowing-txt glowing-texts' id= 'Ресторан'>Рес<span class='faulty-letter' id= 'Ресторан'>тор</span>ан</button>
+        <button class='glowing-btn glowing-batton' id= 'Кофейня'><span class='glowing-txt glowing-texts' id= 'Кофейня'>Коф<span class='faulty-letter' id= 'Кофейня'>ей</span>ня</button>
+        <button class='glowing-btn glowing-batton' id= 'Кафе с завтраками'><span class='glowing-txt glowing-texts'id= 'Кафе с завтраками'>кафе<span class='faulty-letter'id= 'Кафе с завтраками'> с завт</span>рака<span class='faulty-letter'id= 'Кафе с завтраками'>ми</span></button>
+    </article> 
+`
+checkEats(eventChange)
+}
